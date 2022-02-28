a675ffde2d17ce9d2a3815052d786aebdc1c6eca
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-02-27 21:23:16 -0500

    added more nsfw detection

--- utils/nsfwdetector.js
+++ utils/nsfwdetector.js
@@ -2,12 +2,18 @@ const axios = require('axios').default
 function isNsfwQ(url, message){
     try {
     axios.get(`https://nsfw-demo.sashido.io/api/image/classify?url=${url}`).then((response, err) => {
+        //console.log(response.data)
         if(err) { throw err }
         if(response.data[0].className === 'Hentai' || response.data[0].className === 'Porn'){
                 message.reply(`Your image has been flagged as NSFW, please refrain from posting this kind of stuff`).then(() => {
                     setTimeout(() => message.delete(), 1)
                 })
             
+        }
+        else if(response.data[1].className === 'Hentai' || response.data[1].className === 'Porn'){
+            message.reply(`Your image has been flagged as NSFW, please refrain from posting this kind of stuff`).then(() => {
+                setTimeout(() => message.delete(), 1)
+            })
         }
     })
 } catch(err) {
