e15443296a5dc28cc68bf67308afa79e38d92f7d
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-02-27 21:46:53 -0500

    video handler

--- utils/nsfwdetector.js
+++ utils/nsfwdetector.js
@@ -4,17 +4,19 @@ function isNsfwQ(url, message){
     axios.get(`https://nsfw-demo.sashido.io/api/image/classify?url=${url}`).then((response, err) => {
         //console.log(response.data)
         if(err) { throw err }
+        try {
         if(response.data[0].className === 'Hentai' || response.data[0].className === 'Porn'){
                 message.reply(`Your image has been flagged as NSFW, please refrain from posting this kind of stuff`).then(() => {
                     setTimeout(() => message.delete(), 1)
                 })
             
         }
-        else if(response.data[1].className === 'Hentai' || response.data[1].className === 'Porn'){
+    } catch(err){}
+        /*else if(response.data[1].className === 'Hentai' || response.data[1].className === 'Porn'){
             message.reply(`Your image has been flagged as NSFW, please refrain from posting this kind of stuff`).then(() => {
                 setTimeout(() => message.delete(), 1)
             })
-        }
+        }*/
     })
 } catch(err) {
     console.log(err)
