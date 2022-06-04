62a7157614e7df81372052af74118b3b558fba42
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-06-04 14:43:18 -0400

    fix: Codebase and typo

--- slash/general/channel-info.js
+++ slash/general/channel-info.js
@@ -1,4 +1,18 @@
 const { Discord, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const channelTypes = {
+	GUILD_TEXT: 0,
+	DM: 1,
+	GUILD_VOICE: 2,
+	GROUP_DM: 3,
+	GUILD_CATEGORY: 4,
+	GUILD_NEWS: 5,
+	GUILD_NEWS_THREAD: 10,
+	GUILD_PUBLIC_THREAD: 11,
+	GUILD_PRIVATE_THREAD: 12,
+	GUILD_STAGE_VOICE: 13,
+	GUILD_DIRECTORY: 14,
+	GUILD_FORUM: 15
+}
 
 module.exports = {
 	name: 'channel-info',
@@ -12,63 +26,22 @@ module.exports = {
 		},
 	],
 	category: 'general',
-	/**
-	 *
-	 * @param {*} interaction
-	 */
 	run: async (interaction) => {
 		const channel = interaction.options.getChannel('channel');
-		console.log(channel)
 		const embed = new EmbedBuilder().setTitle(`${channel.name} Info`);
-		if (channel.isText && channel.topic) {
-			embed.setDescription(channel.topic);
+		if (channel.isText && channel.topic) embed.setDescription(channel.topic);
+		
+		if (channel.rateLimitPerUser) embed.addField({ name: 'Slow Mode:', value: `${channel.rateLimitPerUser} Seconds` });
+		if (channel.parent) embed.addFields({ name: 'Catgory Name:', value: channel.parent.name });
+		if (channel.lastPinTimestamp) embed.addFields({ name: 'Last Pin Message At:', value: `<t:${Math.floor(channel.lastPinTimestamp / 1000)}:R>`})
+		if (channel.nsfw) embed.addFields({ name: 'Channel NSFW?', value: 'Yes' });
+		for(const [key, value] of Object.entries(channelTypes)){
+			if(channel.type == value){ 
+				var type = key
+				break;
+			}
 		}
-		if (channel.rateLimitPerUser) {
-			embed.addField({ name: 'Slow Mode:', value: `${channel.rateLimitPerUser} Seconds` });
-		}
-		if (channel.parent) {
-			embed.addFields({ name: 'Catgory Name:', value: channel.parent.name });
-		}
-		if (channel.lastPinTimestamp) {
-			embed.addFields(
-				{ name: 'Last Pin Message At:', value: `<t:${Math.floor(channel.lastPinTimestamp / 1000)}:R>`}
-			)
-		}
-		if (channel.nsfw) {
-			embed.addFields({ name: 'Channel NSFW?', value: 'Yes' });
-		}
-		let channelTypes;
-		switch (channel.type) {
-			case 0:
-				channelTypes = 'Text Channel';
-				break;
-			case 2:
-				channelTypes = 'Voice Channel';
-				break;
-			case 4:
-				channelTypes = 'Category Channel';
-				break;
-			case 5:
-				channelTypes = 'News Channel';
-				break;
-			case 10:
-				channelTypes = 'News Thread Channel';
-				break;
-			case 11:
-				channelTypes = 'Public Thread Channel';
-				break;
-			case 12:
-				channelTypes = 'Private Thread Channel';
-				break;
-			case 13:
-				channelTypes = 'Stage Channel';
-				break;
-			default: 
-				channelTypes = 'Cannot find channel type'
-				break;
-		}
-		console.log(channelTypes)
-		embed.addFields({ name: 'Channel Type:', value: channelTypes, inline: true })
+		embed.addFields({ name: 'Channel Type:', value: type, inline: true })
 		embed.addFields({ name: 'Channel Created At:', value: `<t:${Math.floor(channel.createdTimestamp / 1000)}:R>`, inline: true });
 		embed.setColor(interaction.guild.me.displayHexColor);
 		
@@ -79,7 +52,7 @@ module.exports = {
 				.setLabel('Members With Access To Channel')
 				.setEmoji({ name:'👩‍👧‍👦' }),
 		);
-		interaction.reply({
+		await interaction.reply({
 			embeds: [embed],
 			components: [row],
 		});
@@ -93,14 +66,8 @@ module.exports = {
 						content: `:x: I can\'t find any members with access in this channel (or not cached members)`,
 					});
 				}
-				let loopedMembers = '';
-				let num = 0;
-				channel.members.forEach((member) => {
-					num++;
-					loopedMembers += `**#${num}** | ${member}\n`;
-				});
 				i.editReply({
-					content: `**${channel} Members:**\n\n${loopedMembers}`,
+					content: `**${channel} Members:**\n\n${channel.members.map((r) => r).join('')}`,
 				});
 			}
 		});
