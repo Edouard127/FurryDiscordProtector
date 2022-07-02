f903565e346cac7116d6c9cafc7814a5ce0490c0
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-07-02 19:55:16 -0400

    Moved directory

--- utils/getServerCount.js
+++
@@ -1,9 +0,0 @@
-const getServerCount = async (client) => {
-    // get guild collection size from all the shards
-    const req = await client.shard.fetchClientValues("guilds.cache.size");
-
-    // return the added value
-    return req.reduce((p, n) => p + n, 0);
-}
-
-module.exports = getServerCount
