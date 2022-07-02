a3c46c65086646893dfbd4750848dab900be32df
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-07-02 19:56:21 -0400

    Feature: Global Storage

---
+++ src/etc/globalStorage.ts
@@ -0,0 +1,6 @@
+import { Collection } from 'discord.js'
+
+export const slash: Collection<String, NodeRequire> = new Collection()
+export const events: Collection<String, NodeRequire> = new Collection()
+
+
