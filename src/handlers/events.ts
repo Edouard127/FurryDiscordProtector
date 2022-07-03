7a46b47aa96ad49e198f2fcae3de10ab97562622
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-07-02 23:10:33 -0400

    Refactor: Event Directory

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -1,11 +1,11 @@
 import { Client } from 'discord.js';
-const { readdirSync } = require("fs");
-require('colors');
-module.exports = (client: Client) => {
+import { readdirSync } from 'fs'
+import 'colors'
+export default (client: Client) => {
   const load = async(dirs: string) => {
-    const events = readdirSync(`./events/${dirs}/`).filter((d: string) => d.endsWith("js") );
+    const events = readdirSync(`./events/discord/${dirs}/`).filter((d: string) => d.endsWith("js") );
     for (let file of events) {
-      let evt = await import(`../events/${dirs}/${file}`);
+      let evt = await import(`../events/discord/${dirs}/${file}`);
       let eName = file.split('.')[0];
       client.on(eName, evt.bind(null,client));
       console.log('[Events]'.yellow + ` Loaded ` + eName.green + '.');
